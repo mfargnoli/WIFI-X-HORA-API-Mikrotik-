e396e858e26cf76cd44af2e0586bcda3f141db16
TopInstall <darwuinjose@gmail.com>
2023-03-29 12:39:23 -0400

    Update index.php

--- install/index.php
+++ install/index.php
@@ -24,37 +24,37 @@
         <!--  contents area start  -->
         <div class="row">
             <div class="col-md-12">
-                <h4> PHPNuxBill Installer </h4>
-                <h5>Please Read Before Continue</h5>
-                <p><strong>Informasi Aplikasi</strong><br>
-                    Application Name: PHPNuxBill <br>
-                    Release Date: 30/10/2015<br>
-                    By: PHPNuxBill [ <a href="https://github.com/hotspotbilling/phpnuxbill" target="_blank">https://github.com/hotspotbilling/phpnuxbill</a> ]<br>
-                    Donasi Paypal: <b>me@ibnux.et</b><br>
+                <h4> WIFI X HORA Installer </h4>
+                <h5>Por favor, lea antes de continuar</h5>
+                <p><strong>Información de la aplicación</strong><br>
+                    Nombre de la aplicación: WIFI X HORA <br>
+                    Fecha de lanzamiento: 30/10/2023<br>
+                    Por: WIFI X HORA [ <a href="https://github.com/freedarwuin/WIFI-X-HORA-API-Mikrotik-" target="_blank">https://github.com/freedarwuin/WIFI-X-HORA-API-Mikrotik-</a> ]<br>
+                    Donaciones de paypal: <b>darwuinjose@gmai.com</b><br>
                     <br>
-                    <strong>Syarat Penggunaan:</strong><br>
-                    Syarat Penggunaan ini berlaku untuk semua versi.<br><br>
+                    <strong>Condiciones de uso:</strong><br>
+                    Estos Términos de uso se aplican a todas las versiones.<br><br>
                 <ul>
-                    <li>Silahkan Anda menggunakan aplikasi ini dengan bijak, Anda dapat mendesain ulang script maupun tampilan pada
-                        aplikasi ini sesuai dengan kebutuhan anda, memperbayak jumlah copy atau mendistribusikan aplikasi ini.
-                        Dengan catatan tidak menghapus link developer.</li>
-                    <li>Tidak ada garansi dari kami jika anda mengalami error atau merasa rugi ketika menggunakan aplikasi ini,
-                        Anda hanya dapat memberikan feedback yang berisi laporan error, dengan syarat dan ketentuan yang berlaku.</li>
-                    <li>Semua yang terkait biaya atau donasi apapun versi-nya, Anda dapat update seumur hidup atau selama aplikasi
-                        ini masih dikembangkan. Mohon jangan salah pengertian bahwa kami tim pengembang mengkomersilkan produk ini
-                        dan anda membeli produk kami.</li>
-                    <li>Aplikasi ini bersifat sosial untuk dapat dikembangkan bersama. Karena itu kami juga mengundang relawan-relawan
-                        yang mau menjadi pengembangkan aplikasi ini.</li>
-                    <li>Penulis berhak setiap saat untuk mengubah ketentuan Syarat Penggunaan tanpa pemberitahuan sebelumnya.</li>
+                    <li>Utilice esta aplicación con prudencia, puede rediseñar el script o mostrarlo en
+                         esta aplicación según sus necesidades, pague el número de copias o distribuya esta aplicación.
+                         Con el registro de no quitar el enlace de desarrollador.</li>
+                    <li>No ofrecemos ninguna garantía si experimenta un error o siente una pérdida al usar esta aplicación.
+                         Solo puede proporcionar comentarios que contengan informes de errores, con los términos y condiciones que correspondan.</li>
+                     <li>Todo lo relacionado con cuotas o donaciones sin importar la versión, puedes actualizar de por vida o durante la aplicación
+                         todavía se está desarrollando. No malinterprete que nuestro equipo de desarrollo comercializa este producto.
+                         y usted compra nuestro producto.</li>
+                    <li>Esta aplicación es social para ser desarrollada en conjunto. Por eso también invitamos a voluntarios
+                         que quiere ser un desarrollador de esta aplicación.</li>
+                    <li>El autor tiene derecho en cualquier momento a cambiar los términos de los Términos de uso sin previo aviso.</li>
                 </ul>
             </div>
             <div class="col-md-12"><br>
-                <a href="step2.php" class="btn btn-primary">Accept &amp; Continue</a>
+                <a href="step2.php" class="btn btn-primary">Aceptar &amp; Continuar</a>
             </div>
         </div>
         <!--  contents area end  -->
     </div>
-    <div class="footer">Copyright &copy; 2021 PHPNuxBill. All Rights Reserved<br /><br /></div>
+    <div class="footer">Copyright &copy; 2023 WIFI X HORA. Reservados todos los derechosd<br /><br /></div>
 </body>
 
 </html>
